2305b490d2c4541b89965b764f752a3736826018
RahulvsBhartitoh <bhartimeenaqueen2@gmail.com>
2024-08-03 22:16:59 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning repo...."
+git clone https://github.com/ / /RahulvsBhartitoh /FDSFJKSDFJDHJ
+cd /FDSFJKSDFJDHJ
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
